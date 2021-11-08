1c8140de41a3976ffd6a7db008d92435c947212f
Zahra Ramezani <miss.ramazani@yahoo.com>
2021-11-08 17:14:18 +0330

    First commit

--- src/google/com/Main.java
+++ src/google/com/Main.java
@@ -5,9 +5,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 
 public class Main {
-
     public static void main(String[] args) {
         File file = getDataFolderFromSystem();
         try {
